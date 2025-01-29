be048daf954792e8e6593461482712bf2a3241c6
Higor Ch. <higorchristianfe@gmail.com>
2025-01-29 16:06:11 -0300

    dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,9 @@
 let mix = require('laravel-mix');
 
-mix.js("src/js/app.js", "html/assets/js");
+mix.js("src/js/app.js", "html/assets/js/theme.js");
 
-mix.postCss("src/css/app.css", "html/assets/css", [
-    require("@tailwindcss/postcss"),
+mix.postCss("src/css/app.css", "html/assets/css/app.css", [
+    require("tailwindcss"),
 ]).options({
     processCssUrls: false
 });
