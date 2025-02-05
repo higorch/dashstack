ce240aa6810f23e3c1b4c434164b08ba8e97eddb
Higor Ch. <higorchristianfe@gmail.com>
2025-02-05 11:56:02 -0300

    dropdown

--- src/js/frontend.js
+++ src/js/frontend.js
@@ -28,6 +28,70 @@ Alpine.data('app', () => ({
         }
     }
 }));
+
+Alpine.data('mask', () => ({
+    init() {
+        Inputmask().mask(this.$el);
+    }
+}));
+
+Alpine.data('dropdown', (placement = 'left', strategy = 'absolute', distance = 0) => ({
+    open: false,
+    init() {
+        var referenceEl = this.$refs.referenceDropdown;
+        var floatingEl = this.$refs.floatingDropdown;
+        autoUpdate(referenceEl, floatingEl, () => {
+            computePosition(referenceEl, floatingEl, {
+                strategy: strategy,
+                placement: placement,
+                middleware: [flip(), offset(distance)]
+            }).then(({ x, y }) => {
+                Object.assign(floatingEl.style, {
+                    top: `${y}px`,
+                    left: `${x}px`
+                });
+            });
+        });
+    }
+}));
+
+Alpine.data('modal', (ref) => ({
+    open: false,
+    ref: ref,
+    events: {
+        ['@open-modal.window']() {
+            if (this.$event.detail.ref == this.ref) this.open = true;
+        },
+        ['@close-modal.window']() {
+            if (this.$event.detail.ref == this.ref) this.open = false;
+        }
+    },
+    init() {
+        var zIndexCounter = 1000;
+
+        this.$watch('open', (value) => {
+            document.body.style.overflow = value ? 'hidden' : 'auto';
+            // trigger event on close modal            
+            if (value === false) this.$dispatch('closed.' + this.ref);
+            // trigger event on open modal
+            else if (value === true) this.$dispatch('opened.' + this.ref);
+            // calculate z-index automatically
+            if (value === true) {
+                this.$root.style.zIndex = this.maxZIndex + 1;
+            }
+        });
+    },
+    get maxZIndex() {
+        var maxZ = 0;
+        document.querySelectorAll('*').forEach(el => {
+            const zIndex = parseInt(window.getComputedStyle(el).zIndex, 10);
+            if (!isNaN(zIndex) && zIndex > maxZ) {
+                maxZ = zIndex;
+            }
+        });
+        return maxZ;
+    }
+}));
 // END IMPLEMENTATION
 
 Alpine.start();
