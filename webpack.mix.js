2b486d10bd1125d4b0df563400dac8068b89c2d9
Higor Ch. <higorchristianfe@gmail.com>
2025-02-17 15:45:22 -0300

    tailwindcss v4

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,16 @@
 let mix = require('laravel-mix');
 
+mix.webpackConfig({
+    watchOptions: {
+        ignored: /node_modules|mix-manifest.json/,
+    }
+});
+
 mix.js("src/js/frontend.js", "html/assets/js/frontend.js");
 mix.js("src/js/custom.js", "html/assets/js/custom.js");
 
 mix.postCss("src/css/frontend.css", "html/assets/css/frontend.css", [
-    require("tailwindcss"),
+    require("@tailwindcss/postcss"),
 ]).options({
     processCssUrls: false
 });
